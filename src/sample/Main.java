182310ec60251e9f39c73f135e937ff86aed973a
podkydish <aaandreyev007@gmail.com>
2022-03-31 12:46:14 +0300

    Andreev dz

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,29 +3,19 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import sample.components.ElementsFlowPane;
-import sample.components.ElementsGridPane;
-import sample.components.ElementsGroup;
+
 import sample.components.FiguresGroup;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("GUIMainForm");
 
-//        Label helloWorldLabel = new Label("Label on custom scene!");
-//        helloWorldLabel.setAlignment(Pos.CENTER);
-//        Scene primaryScene = new Scene(helloWorldLabel);
-//        primaryStage.setScene(primaryScene);
 
-//        Group root = new FiguresGroup();
-        Group root = new ElementsGroup();
-//        Pane root = new ElementsGridPane();
-//        Pane root = new ElementsFlowPane();
+        Group root = new FiguresGroup();
+
         primaryStage.setScene(new Scene(root, 300, 275));
 
         primaryStage.show();
